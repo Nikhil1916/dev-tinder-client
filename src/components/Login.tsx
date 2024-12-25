fe39c5827bb33db9a0b00ad5c2854ca50470f9c3
nikhil <nikhilchawla9013@gmail.com>
2024-12-25 22:52:19 +0530

    create login component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useEffect, useRef } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
 
-export default Login
+  const handleLogin = async() => {
+    console.log(import.meta.env.VITE_API_URL);
+  }
+
+  useEffect(() => {
+    if (emailRef.current) {
+      emailRef.current.value = "nik@gmail.com";
+    }
+    if (passwordRef.current) {
+      passwordRef.current.value = "nikhiL1234!";
+    }
+  }, []);
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-200 w-96 shadow-xl">
+        <div className="card-body">
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Email</span>
+            </div>
+            <input
+              type="text"
+              className="input input-bordered w-full max-w-xs"
+              ref={emailRef}
+            />
+          </label>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Password</span>
+            </div>
+            <input
+              type="text"
+              className="input input-bordered w-full max-w-xs"
+              ref={passwordRef}
+            />
+          </label>
+          <button className="btn btn-primary my-2" onClick={handleLogin}>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string; // Add your environment variables here
+    // readonly VITE_ANOTHER_VAR: string; // Example for additional variables
+}
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
